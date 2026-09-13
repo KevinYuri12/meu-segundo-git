6b592823d6961697a4a8719f835cb40385003f0f
Kevin Yuri Macera <kevinmacera13@gmail.com>
2026-09-13 12:02:50 -0300

    Função de fatorial implementada!

--- calculador.c
+++ calculador.c
@@ -15,3 +15,13 @@ int subtracao(int a, int b){
 float divisao(float a, float b){
     return a/b;
 }
+
+int fatorial(int valor){
+    if(valor > 0 && valor <= 1){
+        return 1;
+    }else if(valor > 1){
+        return valor * fatorial(valor-1);
+    }else{
+        return 0;
+    }
+}
